d3ab8b0998f3cf7876d1419ed432ebfc2e48f739
Andrei Vovchok <andreivovchok@bk.ru>
2021-09-12 09:38:18 +0300

    Добавил решение задания

---
+++ ClassLibrary1/RecursiveSearcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace ClassLibrary1
+{
+    public class RecursiveSearcher
+    {
+        public FileInfo Search(DirectoryInfo directoryInfo, string fileName)
+        {
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                if (fileName == file.Name)
+                {
+                    return file;
+                }
+            }
+
+            foreach (var directory in directoryInfo.GetDirectories())
+            {
+                try
+                {
+                    return Search(directory, fileName);
+                }
+                catch (FileNotFoundException)
+                {  
+                }
+            }
+
+            throw new FileNotFoundException("File not found", fileName);
+        }
+    }
+}
---
+++ HomeWorkNumbeFifteen/Program.cs
@@ -0,0 +1,31 @@
+﻿using ClassLibrary1;
+using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace HomeWorkNumberFifteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь к папке для поиска!");
+            string path = Console.ReadLine();
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            Console.WriteLine("Введите название файла!");
+            RecursiveSearcher searcher = new RecursiveSearcher();
+            var file = searcher.Search(directoryInfo, Console.ReadLine());
+            
+            var archivePath = Path.Combine(file.Directory.FullName, Path.GetFileNameWithoutExtension(file.FullName) + ".zip");
+
+            using (var fileStream = new FileStream(archivePath, FileMode.Create))
+            using (ZipArchive zipArchive = new ZipArchive(fileStream, ZipArchiveMode.Create))
+            {
+                zipArchive.CreateEntryFromFile(file.FullName, file.Name);
+            }
+
+            Console.WriteLine(archivePath);
+        }
+    }
+}
